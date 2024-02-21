27825bed6c07c428cfe45d8ca42a5e8fe52abae9
avenegas91 <aven2601@yahoo.com>
2024-02-20 22:14:32 -0500

    first commit

---
+++ db.js
@@ -0,0 +1,34 @@
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "jobly",
+    password: "postgres",
+    port: 5432,
+
+    //connectionString: getDatabaseUri(),
+    //ssl: {
+      //rejectUnauthorized: false
+    //}
+  });
+} else {
+  db = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "jobly",
+    password: "postgres",
+    port: 5432,
+    //connectionString: getDatabaseUri()
+  });
+}
+
+db.connect();
+
+module.exports = db;
