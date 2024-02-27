6f1ef627e9e5db64bd76941c84c7624953d5685e
avenegas91 <aven2601@yahoo.com>
2024-02-27 16:22:30 -0500

    Added Middleare functions for administrator usage

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,8 +42,42 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+// ADDED MIDDLEWARE 
+/* Middleware used when logged in as admin user.  If not, raises unauthorized */
+
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user || !res.locals.user.isAdmin) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  }
+  catch (e) {
+    return next(e);
+  }
+}
+
+
+// ADDED MIDDLEWARE
+/* Middleware used when a valid token must be provided to match username in the route param.
+If not, raises unauthorized */
+
+function ensureCorrectUserOrAdmin(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (!(user && (user.isAdmin || user.username === req.params.username))) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  }
+  catch (err) {
+    return next(e);
+  }
+}
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin, // ADDED EXPORT
+  ensureCorrectUserOrAdmin, // ADDED EXPORT
 };
