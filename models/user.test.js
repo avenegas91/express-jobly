633a31b35924b8df9fcac9497b6390247249c397
avenegas91 <aven2601@yahoo.com>
2024-03-03 13:42:46 -0500

    Added a test for applyToJob

--- models/user.test.js
+++ models/user.test.js
@@ -228,3 +228,38 @@ describe("remove", function () {
     }
   });
 });
+
+
+/* ADDED 
+applyTo Job */
+
+describe("applyToJob", () => {
+  test("works", async () => {
+    await User.applyToJob("u1", testJobIds[1]);
+
+    const res = await db.query(
+        "SELECT * FROM applications WHERE job_id=$1", [testJobIds[1]]);
+    expect(res.rows).toEqual([{
+      job_id: testJobIds[1],
+      username: "u1",
+    }]);
+  });
+
+  test("not found if no such job", async () => {
+    try {
+      await User.applyToJob("u1", 0, "applied");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+
+  test("not found if no such user", async () => {
+    try {
+      await User.applyToJob("nope", testJobIds[0], "applied");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
